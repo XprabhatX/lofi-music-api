53c65b78f122f8d05e54a1d6a987b8c38a772856
Prabhat Pankaj <prabhatpankajofficial@gmail.com>
2024-07-12 21:24:28 +0530

    added .env.example

--- controllers/SongControllers.js
+++ controllers/SongControllers.js
@@ -4,7 +4,6 @@ export const getAllSongs = async(req, res) => {
     try {
         const result = await Song.find()
         res.json(result)
-        console.log('reached controller\n' + result)
     } catch (err) {
         console.error('Error in getAllSongs():', err)
         res.status(500).json({ error: 'Internal server error'})
